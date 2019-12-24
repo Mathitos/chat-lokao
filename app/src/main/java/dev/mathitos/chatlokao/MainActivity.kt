fa2e18c55e4167acba40e952a19694c77e49a8a8
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-12-24 16:22:13 -0300

    fix login not being persistent

--- app/src/main/java/dev/mathitos/chatlokao/MainActivity.kt
+++ app/src/main/java/dev/mathitos/chatlokao/MainActivity.kt
@@ -1,18 +1,24 @@
 package dev.mathitos.chatlokao
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 import dev.mathitos.chatlokao.message.MessageEntity
 import dev.mathitos.chatlokao.ui.MessageAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var firebaseDatabase: FirebaseDatabase
-    lateinit var messagesDatabaseReference: DatabaseReference
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var messagesDatabaseReference: DatabaseReference
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
 
     lateinit var messageListAdapter: MessageAdapter
 
@@ -22,14 +28,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Init Firebase database
         firebaseDatabase = FirebaseDatabase.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance()
+
         messagesDatabaseReference = firebaseDatabase.reference.child("messages")
+
+        initMessageAdapter()
 
         sendButton.setOnClickListener {
             sendMessage()
         }
+        addDatabaseEventListener()
+        addAuthStateListener()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        firebaseAuth.removeAuthStateListener(authStateListener)
+    }
+
+    private fun initMessageAdapter() {
+        messageListAdapter = MessageAdapter(this@MainActivity,  mutableListOf())
+        messageList.adapter = messageListAdapter
+    }
+
+    private fun addDatabaseEventListener(){
         messagesDatabaseReference.addChildEventListener( object: ChildEventListener {
             override fun onCancelled(p0: DatabaseError) {
             }
@@ -51,10 +79,17 @@ class MainActivity : AppCompatActivity() {
             override fun onChildRemoved(p0: DataSnapshot) {
             }
         })
+    }
 
-        messageListAdapter = MessageAdapter(this@MainActivity,  mutableListOf())
-
-        messageList.adapter = messageListAdapter
+    private fun addAuthStateListener(){
+        authStateListener = FirebaseAuth.AuthStateListener {
+            firebaseAuth ->
+                if(firebaseAuth.currentUser != null) {
+                    username = firebaseAuth.currentUser!!.displayName!!
+                } else {
+                    createSignInIntent()
+                }
+        }
     }
 
     private fun sendMessage() {
@@ -68,5 +103,62 @@ class MainActivity : AppCompatActivity() {
 
     private fun showToaster ( text: String){
         Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                if(user != null && user.displayName != null ) {
+                    username = user.displayName!!
+                }
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                showToaster("deu ruim no login")
+            }
+        }
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // ...
+            }
+    }
+
+    private fun delete() {
+        AuthUI.getInstance()
+            .delete(this)
+            .addOnCompleteListener {
+                // ...
+            }
+    }
+
+    companion object {
+
+        private const val RC_SIGN_IN = 123
     }
 }
